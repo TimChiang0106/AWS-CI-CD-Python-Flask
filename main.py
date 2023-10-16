48848a0e329a378c71e7fc03981342c50295f99c
Tim Chiang <e12345678988@gmail.com>
2023-10-16 16:56:43 +0800

    0.4

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello Production {}!".format(name)
+
+
+@app.route("/post", methods=['POST'])
+def post_test():
+    cookie = request.values['cookie']
+    return "Hello Production {}!".format(cookie)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=int(os.environ.get("PORT", 8080)))
